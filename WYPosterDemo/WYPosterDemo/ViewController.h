b9db1375b49cec8a09dd505a3b190219de85426a
wyan assert <wyan@wyan-MacBook-Pro.local>
2017-07-21 17:46:37 +0800

    add pod

---
+++ WYPosterDemo/WYPosterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYPoster
+//
+//  Created by wyan assert on 2017/7/21.
+//  Copyright © 2017年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
